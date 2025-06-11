6347bb6be2850fd5574721fd55a9ba84d0befc79
Patchcent <patchrosascent@gmail.com>
2025-06-10 22:30:39 -0600

    Actualiza archivos del proyecto NutriTrack

--- routes/historial_peso.py
+++ routes/historial_peso.py
@@ -25,3 +25,12 @@ async def registrar_peso(peso_data: PesoRegistro):
     })
 
     return {"id": str(result.inserted_id), "message": "Peso registrado"}
+@router.get("/{email}")
+async def obtener_historial_peso(email: str):
+    registros = []
+    cursor = db.historial_peso.find({"email": email}).sort("fecha", 1)
+    async for doc in cursor:
+        doc["id"] = str(doc["_id"])
+        del doc["_id"]
+        registros.append(doc)
+    return registros
